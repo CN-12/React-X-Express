3506e737874eaa93a2a888ee75e94f94477da37d
J1min <jmhong101506@gmail.com>
2021-11-16 16:18:46 +0900

    이렇게연동하면되용

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8080;
+app.use(express.json());
+var cors = require("cors");
+app.use(cors());
 
 app.listen(port, function () {
   console.log(`Listening on http://localhost:${port}`);
 });
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname , "/frontend/build/index.html"));
 });
