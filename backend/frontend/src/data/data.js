d65e7e9858ddca5414001dc4cea84fc1e4733a53
J1min <jmhong101506@gmail.com>
2021-11-16 16:34:34 +0900

    데이터 이름 수정

--- backend/frontend/src/data/data.js
+++ backend/frontend/src/data/data.js
@@ -8,11 +8,11 @@ const data = [
     image: "https://ifh.cc/g/E8Sr3C.jpg",
   },
   {
-    name: "동분서주 - 시라사",
+    name: "동분서주 - 시라사기 치사토",
     image: "https://ifh.cc/g/f1swMT.jpg",
   },
   {
-    name: "나의 무사도! - 와카",
+    name: "나의 무사도! - 와카미야 이브",
     image: "https://ifh.cc/g/IE7RoC.jpg",
   },
   {
@@ -20,35 +20,35 @@ const data = [
     image: "https://ifh.cc/g/ma3a4V.jpg",
   },
   {
-    name: "천문부에 어서 오세요! - 히카",
+    name: "천문부에 어서 오세요! - 히카와 히나",
     image: "https://ifh.cc/g/3P3Ty5.jpg",
   },
   {
-    name: "이걸로 완벽해! - 아야",
+    name: "이걸로 완벽해! - 마루야마 아야",
     image: "https://ifh.cc/g/z3ELsF.jpg",
   },
   {
-    name: "Special birthday! - 히나",
+    name: "Special birthday! - 히카와 히나",
     image: "https://ifh.cc/g/MbPA01.jpg",
   },
   {
-    name: "서로 웃을 수 있는 존재 - 아야",
+    name: "서로 웃을 수 있는 존재 - 마루야마 아야",
     image: "https://ifh.cc/g/KTBriR.jpg",
   },
   {
-    name: "아침노을에 싸여 - 아야",
+    name: "아침노을에 싸여 - 마루야마 아야",
     image: "https://ifh.cc/g/gBuCVX.jpg",
   },
   {
-    name: "나답게 즐겁게 - 마야",
+    name: "나답게 즐겁게 - 야마토 마야",
     image: "https://ifh.cc/g/H0M46b.jpg",
   },
   {
-    name: "샹들리에 로드",
+    name: "샹들리에 로드 - 와카미야 이브",
     image: "https://ifh.cc/g/uRJPRG.jpg",
   },
   {
-    name: "준비는 완벽합니다!",
+    name: "준비는 완벽합니다! - 야마토 마야",
     image: "https://ifh.cc/g/29ckTP.jpg",
   },
 ];
