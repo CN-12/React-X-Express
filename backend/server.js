ef4a09d52a197f330dc6ebb2148a508ae6ca0389
J1min <jmhong101506@gmail.com>
2021-11-16 12:56:53 +0900

    일단은 여기까지 킵

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
-const http = require("http").createServer(app);
-http.listen(port, function () {
+app.listen(port, function () {
   console.log(`Listening on http://localhost:${port}`);
 });
 
-app.use(express.static(path.join(__dirname, "frontend/build/")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.get("/", (req, res) => {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
